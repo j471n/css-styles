3e645927a73c7e36999fcfb008c3c2f766482209
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-03 14:04:57 +0530

    Adding Button Route and the Button Component
     - Button Component to create the button and copy its style when clicked
     - Buttons Route  where we render the All the buttons and the other components

--- src/Routes/Buttons.jsx
+++ src/Routes/Buttons.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../layout/Navbar";
 import Footer from "../layout/Footer";
+import Greeting from "../components/Greeting";
+import Button from "../components/Button";
+import { buttons } from "../data/buttons";
 const Buttons = () => {
     return (
         <div>
-            <Navbar></Navbar>
-            <Footer></Footer>
+            <Navbar />
+            <div className="boxShadow_container">
+                <Greeting title="Buttons" />
+                <div className="block-container">
+                    {buttons.map((btn) => {
+                        return (
+                            <Button
+                                key={btn.key}
+                                id={btn.key}
+                                copy={btn.copy}
+                            />
+                        );
+                    })}
+                </div>
+                <Footer />
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Buttons
+export default Buttons;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import Modal from "./Modal";
 
-const Button = () => {
+const Button = (props) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    function copyToClipboard(string) {
+        navigator.clipboard.writeText(string);
+        setIsModalOpen(true);
+        setTimeout(() => {
+            setIsModalOpen(false);
+        }, 1000);
+    }
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <React.Fragment>
+            <button
+                className={`btn btn-${props.id}`}
+                onClick={() => copyToClipboard(props.copy)}
+            >
+                {`Button ${props.id}`}
+            </button>
+            <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                {`Button ${props.id} Copied!`}
+            </Modal>
+        </React.Fragment>
+    );
+};
 
-export default Button
+export default Button;
