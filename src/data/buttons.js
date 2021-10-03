54b82a20cabb159ca10e023ad52e61e7834f0b8f
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-03 14:05:35 +0530

    Button Data

---
+++ src/data/buttons.js
@@ -0,0 +1,314 @@
+export const buttons = [
+    {
+        key: 1,
+        copy: `
+        .btn-1 {
+            padding: 0.6rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : red;
+            color : white;
+            border: 2px solid red;
+        }
+        .btn-1:hover {
+            background-color : white;
+            color : red;
+            border: 2px solid red;
+        }`,
+    },
+    {
+        key: 2,
+        copy: `
+        .btn-2 {
+            padding: 0.6rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : black;
+            color : white;
+            border: 2px solid black;
+        }
+        .btn-2:hover {
+            background-color : white;
+            color : black;
+            border: 2px solid black;
+        }`,
+    },
+    {
+        key: 3,
+        copy: `
+        .btn-3 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : ;
+            color : white;
+            border: 2px solid red;
+        }
+        .btn-3:hover {
+            background-color : white;
+            color : red;
+        }`,
+    },
+    {
+        key: 4,
+        copy: `
+        .btn-4 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #eed202;
+            color : white;
+            border: 2px solid #eed202;
+        }
+        .btn-4:hover {
+            background-color : white;
+            color : #eed202;
+        }`,
+    },
+    {
+        key: 5,
+        copy: `
+        .btn-5 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #2EA44F;
+            color : white;
+            border: 2px solid #2EA44F;
+        }
+        .btn-5:hover {
+            background-color : white;
+            color : #2EA44F;
+        }`,
+    },
+    {
+        key: 6,
+        copy: `
+        .btn-6 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #1652F0;
+            color : white;
+            border: 2px solid #1652F0;
+        }
+        .btn-6:hover {
+            background-color : white;
+            color : #1652F0;
+        }`,
+    },
+    {
+        key: 7,
+        copy: `
+        .btn-7 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #0A66C2;
+            color : white;
+            border: 2px solid #0A66C2;
+        }
+        .btn-7:hover {
+            background-color : white;
+            color : #0A66C2;
+        }`,
+    },
+    {
+        key: 8,
+        copy: `
+        .btn-8 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #405CF5;
+            color : white;
+            border: 2px solid #405CF5;
+        }
+        .btn-8:hover {
+            background-color : white;
+            color : #405CF5;
+        }`,
+    },
+    {
+        key: 9,
+        copy: `
+        .btn-9 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #FAFBFC;
+            color : black;
+            border: 2px solid #FAFBFC;
+            box-shadow: 0 0 3px black;
+        }
+        .btn-10:hover {
+            background-color : black;
+            color : #FAFBFC;
+        }`,
+    },
+    {
+        key: 10,
+        copy: `
+        .btn-10 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #0066FF;
+            color : white;
+            border: 2px solid #0066FF;
+        }
+        .btn-10:hover {
+            background-color : white;
+            color : #0066FF;
+        }`,
+    },
+    {
+        key: 11,
+        copy: `
+        .btn-11 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #2C445A;
+            color : white;
+            border: 2px solid #2C445A;
+        }
+        .btn-11:hover {
+            background-color : white;
+            color : #2C445A;
+        }`,
+    },
+    {
+        key: 12,
+        copy: `
+        .btn-12 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #fff;
+            color : #111;
+            border: none;
+            box-shadow: 2px 2px rgba(0, 0, 0, 0.7);
+        }`,
+    },
+    {
+        key: 13,
+        copy: `
+        .btn-13 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #fff;
+            color : #111;
+            border: none;
+            box-shadow: 2px 2px 50px -10px rgba(0, 0, 0, 0.7);
+        }`,
+    },
+    {
+        key: 14,
+        copy: `
+        .btn-14 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #fff;
+            color : #111;
+            border: none;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.7);
+        }`,
+    },
+    {
+        key: 15,
+        copy: `
+        .btn-15 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #fff;
+            color : #111;
+            border: none;
+            box-shadow: 0 0 5px -2px rgba(0, 0, 0, 0.7);
+        
+        }`,
+    },
+    {
+        key: 16,
+        copy: `
+        .btn-16 {
+            padding: 0.7rem 1.5rem;
+            border: none;
+            font-size: 1em;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 20px;
+            transition: 100ms ease-in-out;
+            background-color : #d3d3d3;
+            color : #111;
+            border : none;
+            box-shadow: 0 0 5px -2px rgba(0, 0, 0, 0.7);
+        }
+        .btn-16:hover {
+            opacity: 0.7;
+        }`,
+    },
+];
